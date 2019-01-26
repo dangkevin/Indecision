ec638050ee2ec4ad712970617f9a03d386d74469
Kevin Dang <kevin@Kevins-Mac-mini.local>
2019-01-25 16:35:02 -0800

    Updated

--- src/components/IndecisionApp.js
+++ src/components/IndecisionApp.js
@@ -61,27 +61,26 @@ export default class IndecisionApp extends React.Component {
     console.log('componentWillUnmount');
   }
   render() {
-    const subtitle = 'Leave your choices to chance!';
+    const subtitle = 'Put your life in the hands of a computer';
 
     return (
       <div>
         <Header subtitle={subtitle} />
-        <div className = "container">
+        <div className="container">
           <Action
-          hasOptions={this.state.options.length > 0}
-          handlePick={this.handlePick}
-        />
-      <div className = "widget">
-        <Options
-        options={this.state.options}
-        handleDeleteOptions={this.handleDeleteOptions}
-        handleDeleteOption={this.handleDeleteOption}
-        />
-        <AddOption
-        handleAddOption={this.handleAddOption}
-        />
-      </div>
-
+            hasOptions={this.state.options.length > 0}
+            handlePick={this.handlePick}
+          />
+          <div className="widget">
+            <Options
+              options={this.state.options}
+              handleDeleteOptions={this.handleDeleteOptions}
+              handleDeleteOption={this.handleDeleteOption}
+            />
+            <AddOption
+              handleAddOption={this.handleAddOption}
+            />
+          </div>
         </div>
         <OptionModal
           selectedOption={this.state.selectedOption}
--- src/components/OptionModal.js
+++ src/components/OptionModal.js
@@ -7,11 +7,11 @@ const OptionModal = (props) => (
     onRequestClose={props.handleClearSelectedOption}
     contentLabel="Selected Option"
     closeTimeoutMS={200}
-    className = "modal"
+    className="modal"
   >
-    <h3 className = "modal__title">Selected Option</h3>
-    {props.selectedOption && <p className = "modal__body">{props.selectedOption}</p>}
-    <button className = "button" onClick={props.handleClearSelectedOption}>Okay</button>
+    <h3 className="modal__title">Selected Option</h3>
+    {props.selectedOption && <p className="modal__body">{props.selectedOption}</p>}
+    <button className="button" onClick={props.handleClearSelectedOption}>Okay</button>
   </Modal>
 );
 
